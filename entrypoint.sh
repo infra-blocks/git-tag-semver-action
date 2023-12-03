3485cf9daaba9b22c75a62687d609aee2413c5d0
PhilLavoie <PhilLavoie@users.noreply.github.com>
2023-12-03 10:32:51 -0500

    Configure safe directories in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+# Configure git to work with repos that are shared through volume in actions runtime.
+# This is done in the entrypoint as opposed to the docker image because the HOME folder is overridden as a
+# shared volume at runtime.
+git config --global --add safe.directory /github/workspace
+
 env INPUT_GITHUB-TOKEN="${1}" INPUT_VERSION="${2}" node /action/dist/index.js

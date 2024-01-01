5fbd0535412d2c2fd1d00197615390723286ed6f
PhilLavoie <PhilLavoie@users.noreply.github.com>
2024-01-01 07:54:09 -0500

    self-test
    Closes #27

--- test/unit/version.spec.ts
+++ test/unit/version.spec.ts
@@ -1,19 +1,37 @@
-import { parseVersion } from "../../src/version.js";
+import { getVersionTags } from "../../src/version.js";
 import { expect } from "@infra-blocks/test";
 
 describe("version", () => {
-  describe(parseVersion.name, function () {
-    it("should work for patch", function () {
-      expect(parseVersion("patch")).to.equal("patch");
+  describe(getVersionTags.name, function () {
+    it("should work for a version of 1.2.3 and a release type of patch", function () {
+      const currentVersion = "1.2.3";
+      const releaseType = "patch";
+
+      expect(getVersionTags({ currentVersion, releaseType })).to.deep.equals([
+        "v1",
+        "v1.2",
+        "v1.2.4",
+      ]);
     });
-    it("should work for minor", function () {
-      expect(parseVersion("minor")).to.equal("minor");
+    it("should work for a version of 1.2.3 and a release type of minor", function () {
+      const currentVersion = "1.2.3";
+      const releaseType = "minor";
+
+      expect(getVersionTags({ currentVersion, releaseType })).to.deep.equals([
+        "v1",
+        "v1.3",
+        "v1.3.0",
+      ]);
     });
-    it("should work for major", function () {
-      expect(parseVersion("major")).to.equal("major");
-    });
-    it("should throw for anything else", function () {
-      expect(() => parseVersion("no version")).to.throw();
+    it("should work for a version of 1.2.3 and a release type of major", function () {
+      const currentVersion = "1.2.3";
+      const releaseType = "major";
+
+      expect(getVersionTags({ currentVersion, releaseType })).to.deep.equals([
+        "v2",
+        "v2.0",
+        "v2.0.0",
+      ]);
     });
   });
 });
